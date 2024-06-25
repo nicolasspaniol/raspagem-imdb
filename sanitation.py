43b33f67bb7dd03aa34a216bceadc6c49374f857
nmspaniol <nicolasmspaniol@gmail.com>
2024-06-25 15:06:33 -0300

    Coloca __name__ == __main__ no sanitation.py

--- sanitation.py
+++ sanitation.py
@@ -19,21 +19,22 @@ def sanitize_number(num):
     return re.sub(r"[^0-9.]", "", str(num))
 
 
-# Importa os dados
-dirty_df = pd.read_csv("reviews.csv")
-
-# Utiliza a função `sanitize_string` para limpar os campos de texto da base de dados
-clean_df = dirty_df.copy()
-clean_df["title"] = dirty_df["title"].apply(sanitize_string)
-clean_df["comment"] = dirty_df["comment"].apply(sanitize_string)
-
-# Utiliza a função `sanitize_number` para limpar os campos numéricos da base de dados
-clean_df["rating"] = dirty_df["rating"].apply(sanitize_number)
-clean_df["helpful_count"] = dirty_df["helpful_count"].apply(sanitize_number)
-clean_df["helpful_total"] = dirty_df["helpful_total"].apply(sanitize_number)
-
-# Garante que não haja linhas duplicadas
-clean_df = clean_df.drop_duplicates()
-
-# Salva os dados em um arquivo .csv
-clean_df.to_csv("reviews_limpos.csv", index=False)
+if __name__ == "__main__":
+    # Importa os dados
+    dirty_df = pd.read_csv("reviews.csv")
+    
+    # Utiliza a função `sanitize_string` para limpar os campos de texto da base de dados
+    clean_df = dirty_df.copy()
+    clean_df["title"] = dirty_df["title"].apply(sanitize_string)
+    clean_df["comment"] = dirty_df["comment"].apply(sanitize_string)
+    
+    # Utiliza a função `sanitize_number` para limpar os campos numéricos da base de dados
+    clean_df["rating"] = dirty_df["rating"].apply(sanitize_number)
+    clean_df["helpful_count"] = dirty_df["helpful_count"].apply(sanitize_number)
+    clean_df["helpful_total"] = dirty_df["helpful_total"].apply(sanitize_number)
+    
+    # Garante que não haja linhas duplicadas
+    clean_df = clean_df.drop_duplicates()
+    
+    # Salva os dados em um arquivo .csv
+    clean_df.to_csv("clean_reviews.csv", index=False)
