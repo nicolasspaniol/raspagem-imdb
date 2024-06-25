bfb63c132f31179c14f0527c954624b4ac6d9c57
nmspaniol <nicolasmspaniol@gmail.com>
2024-06-25 14:50:19 -0300

    Consistência no código

--- sanitation.py
+++ sanitation.py
@@ -1,42 +1,39 @@
-
 """ Módulo responsável pela higienização dos dados extraidos no scraping """
 
 import pandas as pd
 import re
 
-def limparstr(string):
+
+def sanitize_string(string):
     """
     Limpa a string, excluindo caracteres repetidos
     """
-    # Complia uma expressoes, que identifica caracteres iguais em sequencia
-    expression = re.compile(r'(.)\1{2,}')
-    # Aplica a expressão
-    str_limpa = expression.sub(r'\1', str(string))
+    str_limpa = re.sub(r"(.)\1{2,}", r"\1", string)
     return str_limpa.strip()
 
-def limpanum(num):
+
+def sanitize_number(num):
     """
-    Limpa as strings numericas, garantindo que contenham apenas números
+    Limpa as strings numéricas, garantindo que contenham apenas números
     """
-    str_limpa = re.sub(r'[^0-9.]', '', str(num))
-    return strlimpa
+    return re.sub(r"[^0-9.]", "", str(num))
 
-## Importa os dados
-dfdirty = pd.read_csv(r"reviews.csv")
 
-# Utiliza a função limpastr para limpar os campos de texto da base de dados
-dfclean = dfdirty.copy()
-dfclean['title'] = dfdirty['title'].apply(limparstr)
-dfclean['comment'] = dfdirty['comment'].apply(limparstr)
-dfclean['has_spoiler'] = dfdirty['has_spoiler'].apply(limparstr)
+# Importa os dados
+dirty_df = pd.read_csv("reviews.csv")
 
-# Utiliza a função limpanum para limpar os campos numéricos da base de dados
-dfclean['rating'] = dfdirty['rating'].apply(limpanum)
-dfclean['helpful_count'] = dfdirty['helpful_count'].apply(limpanum)
-dfclean['helpful_total'] = dfdirty['helpful_total'].apply(limpanum)
+# Utiliza a função `sanitize_string` para limpar os campos de texto da base de dados
+clean_df = dirty_df.copy()
+clean_df["title"] = dirty_df["title"].apply(sanitize_string)
+clean_df["comment"] = dirty_df["comment"].apply(sanitize_string)
 
-## Garante que não haja linhas duplicadas
-dfclean = dfclean.drop_duplicates()
+# Utiliza a função `sanitize_number` para limpar os campos numéricos da base de dados
+clean_df["rating"] = dirty_df["rating"].apply(sanitize_number)
+clean_df["helpful_count"] = dirty_df["helpful_count"].apply(sanitize_number)
+clean_df["helpful_total"] = dirty_df["helpful_total"].apply(sanitize_number)
 
-## Salva os dados em um arquivo .csv
-dfclean.to_csv(r"reviews_limpos.csv", index=False)
+# Garante que não haja linhas duplicadas
+clean_df = clean_df.drop_duplicates()
+
+# Salva os dados em um arquivo .csv
+clean_df.to_csv("reviews_limpos.csv", index=False)
